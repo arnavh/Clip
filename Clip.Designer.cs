3591677de039887b2e36528966bd27e22a1c739d
Emmett McFarlane <StaminaCode@Gmail.com>
2015-05-20 19:10:00 -0400

    Updated Clip.cs
    Handles bitmaps in multiple clipboards at once

---
+++ Clip.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Clip
+{
+    partial class frmClip
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlClipboard1 = new System.Windows.Forms.Panel();
+            this.lblClip1 = new System.Windows.Forms.Label();
+            this.pnlClipboard2 = new System.Windows.Forms.Panel();
+            this.lblClip2 = new System.Windows.Forms.Label();
+            this.pnlClipboard3 = new System.Windows.Forms.Panel();
+            this.lblClip3 = new System.Windows.Forms.Label();
+            this.pnlClipboard4 = new System.Windows.Forms.Panel();
+            this.lblClip4 = new System.Windows.Forms.Label();
+            this.pnlClipboard = new System.Windows.Forms.Panel();
+            this.lblClip = new System.Windows.Forms.Label();
+            this.pnlClipboard1.SuspendLayout();
+            this.pnlClipboard2.SuspendLayout();
+            this.pnlClipboard3.SuspendLayout();
+            this.pnlClipboard4.SuspendLayout();
+            this.pnlClipboard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlClipboard1
+            // 
+            this.pnlClipboard1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(189)))), ((int)(((byte)(182)))));
+            this.pnlClipboard1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlClipboard1.Controls.Add(this.lblClip1);
+            this.pnlClipboard1.Location = new System.Drawing.Point(7, 12);
+            this.pnlClipboard1.Name = "pnlClipboard1";
+            this.pnlClipboard1.Size = new System.Drawing.Size(250, 84);
+            this.pnlClipboard1.TabIndex = 0;
+            this.pnlClipboard1.Click += new System.EventHandler(this.pnlClipboard1_Click);
+            this.pnlClipboard1.MouseLeave += new System.EventHandler(this.pnlClipboard1_MouseLeave);
+            this.pnlClipboard1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlClipboard1_MouseMove);
+            // 
+            // lblClip1
+            // 
+            this.lblClip1.AutoSize = true;
+            this.lblClip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lblClip1.Location = new System.Drawing.Point(7, 9);
+            this.lblClip1.MaximumSize = new System.Drawing.Size(230, 0);
+            this.lblClip1.Name = "lblClip1";
+            this.lblClip1.Size = new System.Drawing.Size(30, 13);
+            this.lblClip1.TabIndex = 0;
+            this.lblClip1.Text = "Clip1";
+            // 
+            // pnlClipboard2
+            // 
+            this.pnlClipboard2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(235)))), ((int)(((byte)(187)))));
+            this.pnlClipboard2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlClipboard2.Controls.Add(this.lblClip2);
+            this.pnlClipboard2.Location = new System.Drawing.Point(7, 102);
+            this.pnlClipboard2.Name = "pnlClipboard2";
+            this.pnlClipboard2.Size = new System.Drawing.Size(250, 84);
+            this.pnlClipboard2.TabIndex = 1;
+            this.pnlClipboard2.Click += new System.EventHandler(this.pnlClipboard2_Click);
+            this.pnlClipboard2.MouseLeave += new System.EventHandler(this.pnlClipboard2_MouseLeave);
+            this.pnlClipboard2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlClipboard2_MouseMove);
+            // 
+            // lblClip2
+            // 
+            this.lblClip2.AutoSize = true;
+            this.lblClip2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lblClip2.Location = new System.Drawing.Point(7, 10);
+            this.lblClip2.MaximumSize = new System.Drawing.Size(230, 0);
+            this.lblClip2.Name = "lblClip2";
+            this.lblClip2.Size = new System.Drawing.Size(30, 13);
+            this.lblClip2.TabIndex = 1;
+            this.lblClip2.Text = "Clip2";
+            // 
+            // pnlClipboard3
+            // 
+            this.pnlClipboard3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(158)))), ((int)(((byte)(136)))));
+            this.pnlClipboard3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlClipboard3.Controls.Add(this.lblClip3);
+            this.pnlClipboard3.Location = new System.Drawing.Point(7, 192);
+            this.pnlClipboard3.Name = "pnlClipboard3";
+            this.pnlClipboard3.Size = new System.Drawing.Size(250, 84);
+            this.pnlClipboard3.TabIndex = 1;
+            this.pnlClipboard3.Click += new System.EventHandler(this.pnlClipboard3_Click);
+            this.pnlClipboard3.MouseLeave += new System.EventHandler(this.pnlClipboard3_MouseLeave);
+            this.pnlClipboard3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlClipboard3_MouseMove);
+            // 
+            // lblClip3
+            // 
+            this.lblClip3.AutoSize = true;
+            this.lblClip3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lblClip3.Location = new System.Drawing.Point(7, 13);
+            this.lblClip3.MaximumSize = new System.Drawing.Size(230, 0);
+            this.lblClip3.Name = "lblClip3";
+            this.lblClip3.Size = new System.Drawing.Size(30, 13);
+            this.lblClip3.TabIndex = 3;
+            this.lblClip3.Text = "Clip3";
+            // 
+            // pnlClipboard4
+            // 
+            this.pnlClipboard4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(190)))), ((int)(((byte)(129)))));
+            this.pnlClipboard4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlClipboard4.Controls.Add(this.lblClip4);
+            this.pnlClipboard4.Location = new System.Drawing.Point(7, 282);
+            this.pnlClipboard4.Name = "pnlClipboard4";
+            this.pnlClipboard4.Size = new System.Drawing.Size(250, 84);
+            this.pnlClipboard4.TabIndex = 2;
+            this.pnlClipboard4.Click += new System.EventHandler(this.pnlClipboard4_Click);
+            this.pnlClipboard4.MouseLeave += new System.EventHandler(this.pnlClipboard4_MouseLeave);
+            this.pnlClipboard4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlClipboard4_MouseMove);
+            // 
+            // lblClip4
+            // 
+            this.lblClip4.AutoSize = true;
+            this.lblClip4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lblClip4.Location = new System.Drawing.Point(7, 12);
+            this.lblClip4.MaximumSize = new System.Drawing.Size(230, 0);
+            this.lblClip4.Name = "lblClip4";
+            this.lblClip4.Size = new System.Drawing.Size(30, 13);
+            this.lblClip4.TabIndex = 4;
+            this.lblClip4.Text = "Clip4";
+            // 
+            // pnlClipboard
+            // 
+            this.pnlClipboard.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pnlClipboard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlClipboard.Controls.Add(this.lblClip);
+            this.pnlClipboard.Location = new System.Drawing.Point(7, 372);
+            this.pnlClipboard.Name = "pnlClipboard";
+            this.pnlClipboard.Size = new System.Drawing.Size(250, 84);
+            this.pnlClipboard.TabIndex = 5;
+            // 
+            // lblClip
+            // 
+            this.lblClip.AutoSize = true;
+            this.lblClip.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lblClip.Location = new System.Drawing.Point(7, 12);
+            this.lblClip.MaximumSize = new System.Drawing.Size(230, 0);
+            this.lblClip.Name = "lblClip";
+            this.lblClip.Size = new System.Drawing.Size(24, 13);
+            this.lblClip.TabIndex = 4;
+            this.lblClip.Text = "Clip";
+            // 
+            // frmClip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(264, 469);
+            this.Controls.Add(this.pnlClipboard);
+            this.Controls.Add(this.pnlClipboard4);
+            this.Controls.Add(this.pnlClipboard3);
+            this.Controls.Add(this.pnlClipboard2);
+            this.Controls.Add(this.pnlClipboard1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmClip";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Clip";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pnlClipboard1.ResumeLayout(false);
+            this.pnlClipboard1.PerformLayout();
+            this.pnlClipboard2.ResumeLayout(false);
+            this.pnlClipboard2.PerformLayout();
+            this.pnlClipboard3.ResumeLayout(false);
+            this.pnlClipboard3.PerformLayout();
+            this.pnlClipboard4.ResumeLayout(false);
+            this.pnlClipboard4.PerformLayout();
+            this.pnlClipboard.ResumeLayout(false);
+            this.pnlClipboard.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlClipboard1;
+        private System.Windows.Forms.Label lblClip1;
+        private System.Windows.Forms.Panel pnlClipboard2;
+        private System.Windows.Forms.Label lblClip2;
+        private System.Windows.Forms.Panel pnlClipboard3;
+        private System.Windows.Forms.Label lblClip3;
+        private System.Windows.Forms.Panel pnlClipboard4;
+        private System.Windows.Forms.Label lblClip4;
+        private System.Windows.Forms.Panel pnlClipboard;
+        private System.Windows.Forms.Label lblClip;
+
+    }
+}
+
---
+++ Clip.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Clip
+{
+    public partial class frmClip : Form
+    {
+        object[] Clipboards = new object[4];
+        KeyHook Hook = new KeyHook();
+        Keys[] Binds = { Keys.F6, Keys.F10, Keys.F9, Keys.F8, Keys.F7 };
+        Color Blue = Color.FromArgb(132, 189, 182),
+              Yellow = Color.FromArgb(246, 235, 187),
+              Red = Color.FromArgb(221, 158, 136),
+              Orange = Color.FromArgb(238, 190, 129);
+
+        public frmClip()
+        {
+            InitializeComponent();
+
+            Hook.hook();
+            Hook.KeyDown += new KeyEventHandler(Gkh_KeyDown);
+            foreach (Keys key in Binds)
+            {
+                Hook.HookedKeys.Add(key);
+            }
+
+            if (Clipboard.ContainsText())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Clipboards[i] = Clipboard.GetText().Replace(System.Environment.NewLine, " ");
+                }
+            }
+            else if (Clipboard.ContainsImage())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Clipboards[i] = Clipboard.GetImage();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Clipboards[i] = "null";
+                }
+            }
+
+            Fade("out");
+        }
+
+        Point ScreenSize()
+        {
+            return new Point((int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight);
+        }
+
+        void ShowClipboards()
+        {
+            this.Location = new Point(ScreenSize().X - 268, ScreenSize().Y - 500);
+            this.TopMost = true;
+
+            for (int i = 0; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        if (Clipboards[i].ToString() == "System.Drawing.Bitmap")
+                        {
+                            pnlClipboard1.BackgroundImage = (Bitmap)Clipboards[i];
+                            lblClip1.Text = "";
+                        }
+                        else
+                        {
+                            lblClip1.Text = Clipboards[i].ToString();
+                            pnlClipboard1.BackgroundImage = null;
+                        }
+                        break;
+                    case 1:
+                        if (Clipboards[i].ToString() == "System.Drawing.Bitmap")
+                        {
+                            pnlClipboard2.BackgroundImage = (Bitmap)Clipboards[i];
+                            lblClip2.Text = "";
+                        }
+                        else
+                        {
+                            lblClip2.Text = Clipboards[i].ToString();
+                            pnlClipboard2.BackgroundImage = null;
+                        }
+                        break;
+                    case 2:
+                        if (Clipboards[i].ToString() == "System.Drawing.Bitmap")
+                        {
+                            pnlClipboard3.BackgroundImage = (Bitmap)Clipboards[i];
+                            lblClip3.Text = "";
+                        }
+                        else
+                        {
+                            lblClip3.Text = Clipboards[i].ToString();
+                            pnlClipboard3.BackgroundImage = null;
+                        }
+                        break;
+                    case 3:
+                        if (Clipboards[i].ToString() == "System.Drawing.Bitmap")
+                        {
+                            pnlClipboard4.BackgroundImage = (Bitmap)Clipboards[i];
+                            lblClip4.Text = "";
+                        }
+                        else
+                        {
+                            lblClip4.Text = Clipboards[i].ToString();
+                            pnlClipboard4.BackgroundImage = null;
+                        }
+                        break;
+                    case 4:
+                        if (Clipboard.ContainsImage())
+                        {
+                            pnlClipboard.BackgroundImage = Clipboard.GetImage();
+                            lblClip.Text = "";
+                        }
+                        else
+                        {
+                            lblClip.Text = Clipboard.GetText();
+                            pnlClipboard.BackgroundImage = null;
+                        }
+                        break;
+                }
+            }
+
+            Fade("in");
+        }
+
+        void Fade(string type)
+        {
+            if (type == "in")
+            {
+                while (this.Opacity != 1)
+                {
+                    this.Opacity += 0.01;
+                    Thread.Sleep(2);
+                }
+            }
+            else
+            {
+                while (this.Opacity != 0)
+                {
+                    this.Opacity -= 0.01;
+                    Thread.Sleep(2);
+                }
+            }
+        }
+
+        void Gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Binds[0])
+            {
+                if (this.Opacity == 1)
+                {
+                    Fade("out");
+                }
+                else if (this.Opacity == 0)
+                {
+                    ShowClipboards();
+                }
+            }
+            for (int i = 1; i < 5; i++)
+            {
+                if (e.KeyCode == Binds[i])
+                {
+                    if (Clipboard.ContainsText())
+                        Clipboards[i - 1] = Clipboard.GetText();
+                    else
+                        Clipboards[i - 1] = Clipboard.GetImage();
+                }
+            }
+        }
+
+        #region Panel Colour Changes
+        private void pnlClipboard1_MouseMove(object sender, MouseEventArgs e)
+        {
+            pnlClipboard1.BackColor = Color.FromArgb(Blue.R - 20, Blue.G - 20, Blue.B - 20); ;
+        }
+
+        private void pnlClipboard2_MouseMove(object sender, MouseEventArgs e)
+        {
+            pnlClipboard2.BackColor = Color.FromArgb(Yellow.R - 20, Yellow.G - 20, Yellow.B - 20); ;
+        }
+
+        private void pnlClipboard3_MouseMove(object sender, MouseEventArgs e)
+        {
+            pnlClipboard3.BackColor = Color.FromArgb(Red.R - 20, Red.G - 20, Red.B - 20); ;
+        }
+
+        private void pnlClipboard4_MouseMove(object sender, MouseEventArgs e)
+        {
+            pnlClipboard4.BackColor = Color.FromArgb(Orange.R - 20, Orange.G - 20, Orange.B - 20); ;
+        }
+
+        private void pnlClipboard1_MouseLeave(object sender, EventArgs e)
+        {
+            pnlClipboard1.BackColor = Blue;
+        }
+
+        private void pnlClipboard2_MouseLeave(object sender, EventArgs e)
+        {
+            pnlClipboard2.BackColor = Yellow;
+        }
+
+        private void pnlClipboard3_MouseLeave(object sender, EventArgs e)
+        {
+            pnlClipboard3.BackColor = Red;
+        }
+
+        private void pnlClipboard4_MouseLeave(object sender, EventArgs e)
+        {
+            pnlClipboard4.BackColor = Orange;
+        }
+        #endregion
+
+        #region Panel Clicks
+        private void pnlClipboard1_Click(object sender, EventArgs e)
+        {
+            if (Clipboards[0].ToString() != "System.Drawing.Bitmap")
+                Clipboard.SetText(lblClip1.Text);
+            else
+                Clipboard.SetImage(pnlClipboard1.BackgroundImage);
+        }
+
+        private void pnlClipboard2_Click(object sender, EventArgs e)
+        {
+            if (Clipboards[1].ToString() != "System.Drawing.Bitmap")
+                Clipboard.SetText(lblClip2.Text);
+            else
+                Clipboard.SetImage(pnlClipboard2.BackgroundImage);
+        }
+
+        private void pnlClipboard3_Click(object sender, EventArgs e)
+        {
+            if (Clipboards[2].ToString() != "System.Drawing.Bitmap")
+                Clipboard.SetText(lblClip3.Text);
+            else
+                Clipboard.SetImage(pnlClipboard3.BackgroundImage);
+        }
+
+        private void pnlClipboard4_Click(object sender, EventArgs e)
+        {
+            if (Clipboards[3].ToString() != "System.Drawing.Bitmap")
+                Clipboard.SetText(lblClip4.Text);
+            else
+                Clipboard.SetImage(pnlClipboard4.BackgroundImage);
+        }
+        #endregion
+
+    }
+}
