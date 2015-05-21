6a02655981e9c659a5a030c81ce4ec29e73d1042
Emmett McFarlane <StaminaCode@Gmail.com>
2015-05-20 20:16:08 -0400

    Update Clip.cs

--- Clip.cs
+++ Clip.cs
@@ -7,6 +7,23 @@ using System.Text.RegularExpressions;
 using System.Windows;
 
 // Clip.cs and all source (discluding Keyhook.cs) is intellectual property of Emmett McFarlane
+/*
+Clip allows multiple clipboards with a clean interface and efficient keybinds
+Copyright (C) 2015 Emmett McFarlane
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 namespace Clip
 {
